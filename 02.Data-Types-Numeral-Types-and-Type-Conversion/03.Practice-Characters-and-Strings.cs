1d5685212065d7a907f240bf27991690b56e4b6c
Hristo Hristov (Chris) <hthristov@mail.bg>
2017-04-18 22:01:50 +0300

    Create 03.Practice-Characters-and-Strings.cs

---
+++ 02.Data-Types-Numeral-Types-and-Type-Conversion/03.Practice-Characters-and-Strings.cs
@@ -0,0 +1,22 @@
+using System;
+
+
+namespace _3.PracticeCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = "Software University";
+            char two = 'B';
+            char three = 'y';
+            char four = 'e';
+            string five = "I love programming";
+            Console.WriteLine(one);
+            Console.WriteLine(two);
+            Console.WriteLine(three);
+            Console.WriteLine(four);
+            Console.WriteLine(five);
+        }
+    }
+}
