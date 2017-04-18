2d02ad2f26d1e0fe2e71d9bb72db81d607c79765
Hristo Hristov (Chris) <hthristov@mail.bg>
2017-04-18 22:00:21 +0300

    Create 02.-Practice-Floating-Point-Numbers.cs

---
+++ 02.Data-Types-Numeral-Types-and-Type-Conversion/02.-Practice-Floating-Point-Numbers.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace _02.FloatingPoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal num1 = 3.141592653589793238m;
+            double num2 = 1.60217657d;
+            decimal num3 = 7.8184261974584555216535342341m;
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+        }
+    }
+}
